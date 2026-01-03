42c3550a4b9479cb07a5e56e061840b5a21a5e83
KumariSonal19 <sonalkumari5858@gmail.com>
2026-01-03 14:50:25 +0530

    Added NotificationType

---
+++ notification-service/src/main/java/com/lms/notificationservice/entity/NotificationType.java
@@ -0,0 +1,12 @@
+package com.lms.notificationservice.entity;
+
+public enum NotificationType {
+    LOAN_APPLIED,
+    LOAN_UNDER_REVIEW,
+    LOAN_APPROVED,
+    LOAN_REJECTED,
+    EMI_DUE,
+    EMI_OVERDUE,
+    EMI_PAID,
+    LOAN_CLOSED
+}

fc0d3fc057338e794fd79bc80a19ecb5304158e0
KumariSonal19 <sonalkumari5858@gmail.com>
2026-01-03 12:07:03 +0530

    Added NotificationService

---
+++ loan-service/src/main/java/com/lms/loan/service/NotificationService.java
@@ -0,0 +1,33 @@
+package com.lms.loan.service;
+
+import com.lms.loan.dto.NotificationDTO;
+import com.lms.loan.config.NotificationConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationService {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public void sendNotification(Long userId, Long loanId, String type, String title, String message) {
+        
+        NotificationDTO notification = NotificationDTO.builder()
+                .userId(userId)
+                .loanId(loanId)
+                .type(type)      
+                .title(title)
+                .message(message)
+                .build();
+        
+        rabbitTemplate.convertAndSend(
+            NotificationConfig.EXCHANGE_NAME, 
+            NotificationConfig.ROUTING_KEY, 
+            notification
+        );
+        
+        System.out.println("Message sent to RabbitMQ for User: " + userId);
+    }
+}

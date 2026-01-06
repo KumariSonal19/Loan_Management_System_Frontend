65c37b030e5be93c2424578b7905820fa98df344
KumariSonal19 <sonalkumari5858@gmail.com>
2026-01-06 21:35:46 +0530

    Added environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8087', 
+  endpoints: {
+    auth: '/api/auth',
+    loans: '/api/loans',
+    admin: '/api/admin',
+    emis: '/api/emis',
+    notifications: '/api/notifications'
+  }
+};
